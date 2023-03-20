677dbce520bf133cd108754724cb168e6a70da6a
d <66576231+choyoungwoo9@users.noreply.github.com>
2023-03-21 01:39:03 +0200

    [Silver IV] Title: 듣보잡, Time: 392 ms, Memory: 7444 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+
+int N, M;
+map<string, int> um;
+
+set<string> answer;
+
+int main()
+{
+	cin >> N >> M;
+	for(int i = 0; i < N; i ++)
+	{
+		string str;
+		cin >> str;
+		um[str] = i;
+	}
+	for(int i = 0; i < M; i ++)
+	{
+		string str;
+		cin >> str;
+		if(um.count(str))
+		{
+			answer.insert(str);
+		}
+	}
+	cout << answer.size() << endl;
+	for(auto iter = answer.begin(); iter != answer.end(); iter ++)
+	{
+		cout << *iter << endl;
+	}
+}
