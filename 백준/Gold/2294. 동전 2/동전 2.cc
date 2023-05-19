e084feef870ea168e56cfa3fe48444582edc455a
d <66576231+choyoungwoo9@users.noreply.github.com>
2023-05-19 10:36:50 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -4,31 +4,28 @@ using namespace std;
 
 int n, k;
 
+int INF = 2100000000;
 int dp[10001];
 
 int main()
 {
 	cin >> n >> k;
 
+	for(int i = 1; i <= 10000; i ++)
+		dp[i] = INF;
 	for(int i = 0; i < n; i ++)
 	{
 		int num;
 		cin >> num;
 		if(num <= 10000)
 			dp[num] = 1;
-	}
-
-	for(int i = 2; i <= 10000; i ++)
-	{
-		for(int j = 1; j < i; j++)
+		for(int j = num; j <= 10000; j ++)
 		{
-			if(!dp[j] || !dp[i-j])
-				continue;
-			if(dp[i] == 0 || dp[j] + dp[i-j] < dp[i])
-				dp[i] = dp[j] + dp[i-j];
+			dp[j] = min(dp[j], dp[j-num] + 1);
 		}
 	}
-	if(dp[k] == 0)
+
+	if(dp[k] == INF)
 		cout << "-1\n";
 	else
 		cout << dp[k] << endl;
