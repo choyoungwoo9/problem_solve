f2932582b81ccb5b2c8b4b306c3e4e5a6208487f
d <66576231+choyoungwoo9@users.noreply.github.com>
2023-05-21 10:16:21 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1000001];
+
+int N;
+
+void make_dp()
+{
+	dp[1] = 1;
+	dp[2] = 2;
+	for(int i = 3; i <= N; i ++)
+	{
+		dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+	}
+}
+
+int main()
+{
+	cin >> N;
+	make_dp();
+	cout << dp[N] << endl;
+}
