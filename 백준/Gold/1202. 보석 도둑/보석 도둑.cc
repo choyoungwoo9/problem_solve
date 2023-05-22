a9586a1e54081b4056046224466906ce355b79b8
d <66576231+choyoungwoo9@users.noreply.github.com>
2023-05-22 11:14:02 +0900

    [Gold II] Title: 보석 도둑, Time: 460 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+int bag[300001];
+vector<pair<int, int> >ju;
+priority_queue<int>pq;
+
+int main()
+{
+	cin >> N >> K;
+	for(int i = 0; i < N; i ++)
+	{
+		int w, c;
+		cin >> w >> c;
+		ju.push_back(pair<int, int>(w, c));
+	}
+	for(int i = 0; i < K; i ++)
+	{
+		cin >> bag[i];
+	}
+	sort(bag, bag+K);
+	sort(ju.begin(), ju.end());
+
+
+	int idx = 0;
+	unsigned long long answer = 0;
+	for(int i = 0; i < K; i ++)
+	{
+		for(; idx < N && ju[idx].first <= bag[i]; idx++)
+		{
+			pq.push(ju[idx].second);
+		}
+		if(!pq.empty())
+		{
+			answer += pq.top();
+			pq.pop();
+		}
+	}
+	cout << answer << endl;
+}
