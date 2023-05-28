f64ab509e10dc71f1cd19907ceb06454c262a65a
d <66576231+choyoungwoo9@users.noreply.github.com>
2023-05-28 10:43:06 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 128 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,36 +1,34 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <string>
+
 using namespace std;
 
 int N, M;
-string ar[100001];
-
-map<string, int> mmp;
+unordered_map<string, int>mp1;
+unordered_map<int, string>mp2;
 
 int main()
 {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(NULL); cout.tie(NULL);
 	cin >> N >> M;
-
 	for(int i = 1; i <= N; i ++)
 	{
-		cin >> ar[i];
-		mmp.insert(pair<string, int>(ar[i], i));
+		string str;
+		cin >> str;
+		mp1[str] = i;
+		mp2[i] = str;
 	}
-
-	for(int i = 1; i <= M; i ++)
+	for(int i = 0; i < M; i ++)
 	{
 		string str;
 		cin >> str;
 		if(str[0] >= '0' && str[0] <= '9')
 		{
-			cout << ar[stoi(str)] << "\n";
-		}
+			cout << mp2[stoi(str)] << "\n";
+		}	
 		else
-		{
-			cout << mmp[str] << "\n";
-		}
+			cout << mp1[str] << "\n";
 	}
 }
