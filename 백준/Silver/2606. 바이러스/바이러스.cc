5da7f5690538e7266e0eff7edd4ab64c4be538e6
d <66576231+choyoungwoo9@users.noreply.github.com>
2023-08-07 13:11:17 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,70 +1,39 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int N;
-int answer;
 
-typedef struct relation
-{
-	int visited;
-	int num;
-	relation *next;
-}	relation;
+vector<int> ComVt[101];
+int visited[101];
+int answer = 0;
 
-relation r_pool[10000];
-int pool_index;
-relation index_relation[101];
-relation *get_pool(int num)
+void dfs(int idx)
 {
-	r_pool[pool_index].num = num;
-	r_pool[pool_index].next = NULL;
-	return &r_pool[pool_index++];
-};
-
-void add_relation(int root, int com)
-{
-	relation *com_rel = get_pool(com);
-	com_rel->next = index_relation[root].next;
-	index_relation[root].next = com_rel;
-}
-
-void dfs(int num)
-{
-	if(index_relation[num].visited)
-		return;
-	index_relation[num].visited = 1;
 	answer ++;
-	relation *tmp = index_relation[num].next;
-	while(tmp)
-	{
-		if(!tmp->visited)
-			dfs(tmp->num);
-		tmp = tmp->next;
+	visited[idx] = 1;
+	for(int i = 0; i < ComVt[idx].size(); i++){
+		int next = ComVt[idx][i];
+		if(visited[next])
+			continue;
+		dfs(next);
 	}
 }
 
 int main()
 {
 	cin >> N;
-	int repeat_count;
-	cin >> repeat_count;
-	for(int i = 1; i <= N; i ++)
-	{
-		index_relation[i].visited = 0;
-		index_relation[i].next = NULL;
+	int T;
+	cin >> T;
+	for(int i = 0; i < T; i++){
+		int a, b;
+		cin >> a >> b;
+		ComVt[a].push_back(b);
+		ComVt[b].push_back(a);
 	}
-	for(int i = 0; i < repeat_count; i ++)
-	{
-		int com1, com2;
-		cin >> com1 >> com2;
-		add_relation(com1, com2);
-		add_relation(com2, com1);
-	}
-	if(index_relation[1].next == NULL)
-	{
-		cout << "0" << endl;
-	}
+	visited[1] = 1;
 	dfs(1);
-	cout << answer - 1 << endl;
+	answer --;
+	cout << answer << endl;
 }
