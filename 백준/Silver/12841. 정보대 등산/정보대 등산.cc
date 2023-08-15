1401d3cd194d1f08fbd3c5a95f7212bac61bd545
조영우 <66576231+choyoungwoo9@users.noreply.github.com>
2023-08-15 11:02:52 +0900

    [Silver II] Title: 정보대 등산, Time: 112 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/12841. 정보대 등산/정보대 등산.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int crossAr[100000];
+int leftAr[99999];
+int rightAr[99999];
+
+long long leftSum;
+long long rightSum;
+
+long long answer = 0;
+int idx;
+
+int main()
+{
+	cin >> N;
+	for(int i = 0; i < N; i++)
+		cin >> crossAr[i];
+	for(int i = 0; i < N-1; i++)
+		cin >> leftAr[i];
+	for(int i = 0; i < N-1; i++)
+		cin >> rightAr[i];
+	for(int i = 0; i < N-1; i++)
+		rightSum += rightAr[i];
+	
+	answer = leftSum + rightSum + crossAr[0];
+	idx = 1;
+	for(int i = 0; i < N-1; i++)
+	{
+		leftSum += leftAr[i];
+		rightSum -= rightAr[i];
+		long long curDistance = leftSum + rightSum + crossAr[i+1] ;
+		if(curDistance < answer)
+		{
+			answer = curDistance;
+			idx = i+2;
+		}
+	}
+	cout << idx << " " << answer << endl;
+}
